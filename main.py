f4602e3c3aa4380bdafccf289fde0cd6168281dc
touhu <geekweilai@gmail.com>
2020-04-27 19:03:36 +0800

    0.2.1b
    修复了exe不能显示错误提示的bug
    将输出尺寸改为A4比例

--- main.py
+++ main.py
@@ -6,14 +6,24 @@ try:
     text = file_object.read()
 except FileNotFoundError:
     print("没有找到text.txt，请将要手写的内容写入text.txt并将其放置在与exe程序相同的目录下")
+    print("请自行关闭退出")
+    counter = 1
+    while counter <= 10:
+        counter = counter + 1
+        counter = counter - 1
 else:
     words = text.rstrip()
     num_words = len(words)
     if num_words == 0:
         print("text.txt无内容，请将要手写的内容写入text.txt并保！存！")
+        print("请自行关闭退出")
+        counter = 1
+        while counter <= 10:
+            counter = counter + 1
+            counter = counter - 1
     else:
         template = Template(
-            background=Image.new(mode="1", size=(3000, 5000), color=1),
+            background=Image.new(mode="1", size=(3000, 4243), color=1),
             font_size=100,
             font=ImageFont.truetype("handwrite.ttf"),
             line_spacing=110,
